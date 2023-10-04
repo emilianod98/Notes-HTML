8249a53adad15417caacc982346d92944975fd49
emilianod98 <emidaniele98@gmail.com>
2023-10-04 10:04:55 -0300

    toggle

---
+++ Notes JS/Codigo369/03-Toggle/scripts.js
@@ -0,0 +1,6 @@
+let toggleboton = document.querySelector(".toggle-boton");
+let container = document.querySelector(".container");
+
+toggleboton.onclick=function(){
+    container.classList.toggle("active")
+}
